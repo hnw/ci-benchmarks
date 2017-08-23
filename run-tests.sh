c89beea8d9aaa8150c3bf3e65aacc651a7184480
Yoshio HANAWA <y@hnw.jp>
2017-08-23 22:57:26 +0900

    x

--- run-tests.sh
+++ run-tests.sh
@@ -4,8 +4,14 @@
 [ -n "$TRAVIS" ] && while true; do echo "..."; sleep 60; done &
 
 df -h
-fio ./io-test.fio
-cd byte-unixbench-5.1.3/UnixBench ; ./Run
+
+if [[ $TEST == "fio"  ]]; then
+    fio ./io-test.fio
+fi
+
+if [[ $TEST == "UnixBench"  ]]; then
+    cd byte-unixbench-5.1.3/UnixBench ; ./Run -i 8
+fi
 
 # Travis CI timeout workaround
 [ -n "$TRAVIS" ] && kill %1
